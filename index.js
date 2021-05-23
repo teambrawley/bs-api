bf86323b17c4dcba72e7d7e3901839ab99aa701e
Joe Lee Official <70639311+joeleeofficial@users.noreply.github.com>
2021-05-23 12:42:42 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/*api-bs*/
 exports.get = require('./modules/get.js')
+
+exports.post = require('./modules/post.js')
+
+exports.ws = require('./modules/webSocket.js')
+
+
+/* LICENSE 
+   Apache License */
