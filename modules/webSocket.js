b52691032d00822d77eadf0013b22cbfefb4b171
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2021-09-18 21:37:56 +0800

    Update webSocket.js

--- modules/webSocket.js
+++ modules/webSocket.js
@@ -1,19 +1,29 @@
 'use strict';
 const WebSocket = require('ws')
 const ws = new WebSocket('wss://wscr.is-a.dev');
-const wss = (id) => 
-  new Promise((resolve, reject) => {
+const wss = (options, callback) => {
+
+if(typeof callback !== 'function')callback = function callback(err, result) { return err || result; };
+
+if(!options || typeof options !== 'object')throw new Error('Invalid options');
+
+var type = options.type;
+var tag =  options.tag;
+
+if(!type)throw new Error('Type is undefined');
 
 ws.onopen = function open() {
-
-  console.log('connected');
-  ws.send(id);
+  callback(null, 'connected');
+  ws.send(`${type}-${tag}`);
 };
 
  ws.onmessage = function incoming(message) {
-   resolve({type:'ws-player',data:JSON.parse(message.data)}) 
+   var msg = JSON.parse(message.data);
+   if(msg.message)return callback('Server Returns An Error', null);
+   if(msg.type)throw new Error('Invalid Type');
+   callback(null, {type:`ws-${type}`,data:msg}) 
 };
-})
+}
 
 
 module.exports = wss
